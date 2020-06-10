84f3f36fb49ee547edbc53046a1202ff8227ef71
finicelsia <fini.celsiaa@gmail.com>
2020-06-10 15:46:05 +0800

    Exercise_02

---
+++ pertemuan5/Exercise_02/src/com/multiplatform/Main.java
@@ -0,0 +1,12 @@
+package com.multiplatform;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double myPoundNumber = 100d;
+	    double convertPoundToKilogram = 0.45359237d * myPoundNumber;
+
+        System.out.println("Pound = " +myPoundNumber);
+        System.out.println("Kg = " +convertPoundToKilogram);
+    }
+}
