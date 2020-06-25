76349d1e754e16bd42de445d73289b4096502439
finicelsia <fini.celsiaa@gmail.com>
2020-06-25 16:49:30 +0800

    Exercise 09

---
+++ pertemuan16/Exercise_09/src/com/multiplatform/Circle.java
@@ -0,0 +1,22 @@
+package com.multiplatform;
+
+public class Circle {
+    private double radius;
+
+    //Constructor
+    public Circle(double radius) {
+        if(radius < 0){
+            this.radius = 0;
+        }else{
+            this.radius = radius;
+        }
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return radius*radius*Math.PI;
+    }
+}
