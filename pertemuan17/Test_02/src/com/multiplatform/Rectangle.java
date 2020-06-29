acfc9fc542d7624c20d5963ff5965ebf984be568
finicelsia <fini.celsiaa@gmail.com>
2020-06-29 15:40:08 +0800

    Test 02

---
+++ pertemuan17/Test_02/src/com/multiplatform/Rectangle.java
@@ -0,0 +1,35 @@
+package com.multiplatform;
+
+public class Rectangle {
+    //Fields
+    private double width;
+    private double length;
+
+    //Constructor
+    public Rectangle(double width, double length) {
+        if(width < 0 ){
+            this.width = 0;
+        }
+        else{
+            this.width = width;
+        }
+        if(length < 0){
+            this.length = 0;
+        }
+        else{
+            this.length = length;
+        }
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea() {
+        return getWidth()*getLength();
+    }
+}
